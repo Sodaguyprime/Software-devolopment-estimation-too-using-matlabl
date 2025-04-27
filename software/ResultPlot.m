05663cae48b82886225796fd0f9e949ddd934fb7
r1rxxm <156597356+r1rxxm@users.noreply.github.com>
2025-04-27 21:46:49 +0300

    Added the file for plotting function
    Not as sure about the surface plot, will probably change it to mesh or waterfall

---
+++ software/ResultPlot.m
@@ -0,0 +1,31 @@
+function ResultPlot(fileSize)
+BW = 1:1:100; %Bandwidth
+UploadTime = (fileSize * 8) ./ BW / 60;
+
+figure;
+%for semilogy: 
+subplot(2,1,1);
+semilogy(BW, UploadTime, 'r', 'LineWidth', 1);
+grid on;
+title('Upload Time vs Bandwidth (semilogy plot)');
+xlabel('Bandwidth(Mbps)');
+ylabel('Upload Time(Min)');
+%Normal plot:
+subplot(2,1,2);
+plot(BW, UploadTime, 'b', 'LineWidth', 1);
+grid on;
+title('Upload Time vs Bandwidth (normal plot)');
+xlabel('Bandwidth (Mbps)');
+ylabel('Upload Time (Mins)');
+
+% now for 3rd surface plot: (not sure abt it)
+[X, Y] = meshgrid(1:10:1000, 1:1:100); %LOC vs BW
+Z = (X * 0.05 * 8) ./ Y / 60;
+
+figure;
+surf(X, Y, Z);
+xlabel('LOC');
+ylabel('Bandwidth(Mbps)');
+zlabel('Upload Time (Min)');
+title('Surface Plot of Upload Time');
+grid on;
