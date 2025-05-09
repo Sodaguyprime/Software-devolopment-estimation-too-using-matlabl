30fa5110d41c96e9cf261405295c95121828fcf0
r1rxxm <156597356+r1rxxm@users.noreply.github.com>
2025-05-09 18:27:39 +0300

    Update ValidateInput.m
    fixed minor issues

--- software/ValidateInput.m
+++ software/ValidateInput.m
@@ -1,9 +1,9 @@
-function userinput = ValidateInput(validOpt, DisplayedText)
+function userInput = ValidateInput(validOpt, DisplayedText)
 %main use of this function is to repeatedly prompt the user for inputs till
 %they're valid
-userInput = input(promptText, 's');  %reading input as string for now
-while ~any(strcmp(userInput, validOptions))  % Validate against allowed strings
+userInput = input(DisplayedText, 's');  %reading input as string for now
+while ~any(strcmp(userInput, validOpt))  % Validate against allowed strings
     fprintf('Invalid input. Please try again.\n');
-    userInput = input(promptText, 's'); %recurrs function till while condition is satisfied.
+    userInput = input(DisplayedText, 's'); %recurrs function till while condition is satisfied.
 end
 end
