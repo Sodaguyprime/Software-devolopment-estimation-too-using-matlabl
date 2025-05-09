f5641e1f218ffb1ddcb8ea1ea5a6e4ba5ad8436a
r1rxxm <156597356+r1rxxm@users.noreply.github.com>
2025-05-09 16:39:30 +0300

    Update cocomoEstimation.m
    Now includes matrix (storing a b and d) and a row vector for selection depending on project type

--- software/cocomoEstimation.m
+++ software/cocomoEstimation.m
@@ -1,17 +1,24 @@
 function [effort, duration, TeamSize, KLOC] = cocomoEstimation(LOC, projectType)
 % Calculates effort, duration, and team size based on COCOMO
 
+abd = [2.4,1.05, 0.38; 3.0, 1.12, 0.35; 3.6, 1.20, 0.32];
 c = 2.5; % duration constant
+
 switch projectType
-    case 1
-        a = 2.4; b = 1.05; d = 0.38;
-    case 2
-        a = 3.0; b = 1.12; d = 0.35;
-    case 3
-        a = 3.6; b = 1.20; d = 0.32;
-    otherwise
-        error('Invalid project type.');
+    case 1  % Organic
+        row = 1;
+    case 2  % Semi-detached
+        row = 2;
+    case 3  % Embedded
+        row = 3;
+    otherwise 
+        error('Invalid project type');
 end
+
+a = abd(row, 1);
+b = abd(row, 2);
+d = abd(row, 3);
+
 
 KLOC = LOC / 1000;
 effort = a * (KLOC^b); % in person-months
