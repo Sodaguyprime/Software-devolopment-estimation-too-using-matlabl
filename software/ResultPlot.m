bfe4592ee5e99db06878bd22d07b093b4c6babb5
Ammar Ahmed <152694573+Sodaguyprime@users.noreply.github.com>
2025-05-11 11:47:25 +0200

    Plotting now saves the files as jpg

--- software/ResultPlot.m
+++ software/ResultPlot.m
@@ -2,7 +2,7 @@ function ResultPlot(fileSize)
 BW = 1:1:100; %Bandwidth
 UploadTime = (fileSize * 8) ./ BW / 60;
 
-figure;
+f2 = figure;
 %for semilogy: 
 subplot(2,1,1);
 semilogy(BW, UploadTime, 'r', 'LineWidth', 1);
@@ -22,10 +22,13 @@ ylabel('Upload Time (Mins)');
 [X, Y] = meshgrid(1:10:1000, 1:1:100); %LOC vs BW
 Z = (X * 0.05 * 8) ./ Y / 60;
 
-figure;
+f1 = figure;
 surf(X, Y, Z);
 xlabel('LOC');
 ylabel('Bandwidth(Mbps)');
 zlabel('Upload Time (Min)');
 title('Surface Plot of Upload Time');
 grid on;
+saveas(f1,'surfice plot','jpg');
+saveas(f2,'Upload time','jpg');
+end
